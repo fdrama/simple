c6a62a1abac63c6f979efdbff6b9ddbbd017a7e9
huanghaoa <=>
2022-02-23 18:15:43 +0800

    init

---
+++ src/test/java/com/example/shadow/simpleJavaSpring/SimpleJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shadow.simpleJavaSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleJavaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
