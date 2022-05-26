b6fc9fa949806b9ab78bb97dbef0109348b9deaa
huanghaoa <huanghaoa@zbj.com>
2022-05-26 11:17:44 +0800

    add

---
+++ src/main/java/Hello.java
@@ -0,0 +1,10 @@
+/**
+ * @author huanghaoa
+ * @date 2022年05月26日 11:16
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("hello word");
+    }
+}
